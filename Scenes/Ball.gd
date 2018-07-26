f2d86a8606d6792dcca21bc63c94b4da969485c5
Daniel McPherson <mcphersond@lunainc.com>
2018-07-25 20:58:55 -0400

    Move ball

---
+++ Scenes/Ball.gd
@@ -0,0 +1,15 @@
+extends KinematicBody2D
+
+const SPEED = 8
+
+var motion = Vector2()
+
+func _ready():
+	motion.y = -SPEED
+	motion.x = SPEED / 2
+
+
+func _physics_process(delta):
+	var collision_info = move_and_collide(motion)
+	if collision_info:
+		motion = motion.bounce(collision_info.normal)
