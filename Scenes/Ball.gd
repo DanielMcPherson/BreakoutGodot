0f083d23005cc94ee563cbc213d2c7b8f1f3372c
Daniel McPherson <mcphersond@lunainc.com>
2018-07-26 22:16:42 -0400

    Give ball facial reactions

--- Scenes/Ball.gd
+++ Scenes/Ball.gd
@@ -20,3 +20,22 @@ func _physics_process(delta):
 		if "Brick" in other.get_name():
 			# Tell the brick it has been hit
 			other.hit()
+			# Ball is happy about it
+			if other.position.y < position.y:
+				# Big smile when hitting ball from below
+				$AnimatedSprite.play("big_smile")
+			else:
+				# Murderous glee when hitting ball from above
+				$AnimatedSprite.play("angry")
+		elif "Paddle" in other.get_name():
+			# Ball is happy
+			$AnimatedSprite.play("smile")
+	# Ball gets nervouse when falling down
+	if motion.y > 0:
+		if position.y > 620:
+			$AnimatedSprite.play("worried")
+	if motion.y < 0 and position.y > 550:
+		$AnimatedSprite.play("default")
+	# Ball is confused if not moving up and down fast enough
+	if abs(motion.y) < 3:
+		$AnimatedSprite.play("confused")
